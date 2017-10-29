2184aad43f6f06aee3817e881b0f62436541a03d
pastorsj <pastorsj@rose-hulman.edu>
2017-10-28 21:43:37 -0400

    Added gsap/scrollmagic to angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+declare let ScrollMagic: any;
+declare let TweenMax: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'app';
+  controller: any;
+
+  ngOnInit() {
+    // init controller
+    this.controller = new ScrollMagic.Controller();
+  }
+
+  ngAfterViewInit() {
+    const scene = new ScrollMagic.Scene({
+        triggerElement: '#trigger1'
+      })
+      .setTween('#animate1', 0.5, {backgroundColor: 'green', scale: 2.5}) // trigger a TweenMax.to tween
+      .addIndicators({name: '1 (duration: 0)'}) // add indicators (requires plugin)
+      .addTo(this.controller);
+  }
 }
